8973da3864fad6f2cc723f1dfdb86c6a5b164d45
Abdelhamid Sameh <abdelhameedsameh44@gmail.com>
2025-10-01 20:25:15 +0300

    Added colour pallete to tailwind.config and added the logo

---
+++ ZarfTokenWebApp/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#736CED", // main purple
+        secondary: "#6DD3CE", // teal
+        accent: "#C14953", // reddish
+        muted: "#D5CFE1", // soft background
+        info: "#54C6EB", // sky blue
+      },
+    },
+  },
+};
