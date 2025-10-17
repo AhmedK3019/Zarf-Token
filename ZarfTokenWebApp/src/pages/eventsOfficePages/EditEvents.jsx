b5f9aa4c222db643d98bdd6d435af2aa7f536461
amado-888 <mido1772007@gmail.com>
2025-10-17 11:23:37 +0300

    done edit function

---
+++ ZarfTokenWebApp/src/pages/eventsOfficePages/EditEvents.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import EditTrip from "../tripPages/EditTrip";
+import EditBazaar from "../bazaarPages/EditBazaar";
+import EditConference from "../conferencePages/EditConference";
+
+export default function EditEvent() {
+  const { type, id } = useParams();
+
+  const renderEditComponent = () => {
+    switch (type.toLowerCase()) {
+      case "trip":
+        return <EditTrip id={id} />;
+      case "bazaar":
+        return <EditBazaar id={id} />;
+      case "conference":
+        return <EditConference id={id} />;
+      default:
+        return <p>Unknown event type: {type}</p>;
+    }
+  };
+
+  return <div className="p-6">{renderEditComponent()}</div>;
+}
