9324f5203cfc10537b5489c9f926362078711b25
Abdelhamid Sameh <abdelhameedsameh44@gmail.com>
2025-11-11 19:22:06 +0200

    Added Routes for payment methods in Trip and Workshop

--- server/src/routes/tripRoutes.js
+++ server/src/routes/tripRoutes.js
@@ -3,6 +3,7 @@ import express from "express";
 
 const router = express.Router();
 router.post("/createTrip", tripController.createTrip);
+router.post("/payForTrip/:id", tripController.payForTrip);
 router.get("/getAllTrips", tripController.getAllTrips);
 router.get("/getTrip/:id", tripController.getTrip);
 router.put("/updateTrip/:id", tripController.updateTrip);
