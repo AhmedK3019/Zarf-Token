167bf5a1bb21ddd273077cfeee2cfed9e4b9eae0
Abdelhamid Sameh <abdelhameedsameh44@gmail.com>
2025-11-25 22:24:04 +0200

    Changed buttons locations in edit events pages

--- ZarfTokenWebApp/src/pages/bazaarPages/EditBazaar.jsx
+++ ZarfTokenWebApp/src/pages/bazaarPages/EditBazaar.jsx
@@ -417,13 +417,6 @@ const EditBazaar = () => {
 
           <div className="grid grid-cols-2 gap-4">
             <button
-              type="submit"
-              className="w-full rounded-full border border-primary/40 bg-white px-4 py-2 text-lg font-semibold text-primary tracking-wide shadow-[0_12px_24px_rgba(115,108,237,0.25)] transition-transform hover:-translate-y-0.5 hover:bg-secondary/20 hover:shadow-[0_16px_30px_rgba(115,108,237,0.3)]"
-            >
-              Update Bazaar
-            </button>
-
-            <button
               type="button"
               onClick={() => {
                 navigate(-1);
@@ -431,6 +424,12 @@ const EditBazaar = () => {
               className="w-full rounded-full border border-red-500 bg-red-500 px-4 py-2 text-lg font-semibold text-white tracking-wide shadow-[0_12px_24px_rgba(239,68,68,0.25)] transition-transform hover:-translate-y-0.5 hover:bg-red-600 hover:shadow-[0_16px_30px_rgba(239,68,68,0.3)]"
             >
               Cancel
+            </button>
+            <button
+              type="submit"
+              className="w-full rounded-full border border-primary/40 bg-white px-4 py-2 text-lg font-semibold text-primary tracking-wide shadow-[0_12px_24px_rgba(115,108,237,0.25)] transition-transform hover:-translate-y-0.5 hover:bg-secondary/20 hover:shadow-[0_16px_30px_rgba(115,108,237,0.3)]"
+            >
+              Update Bazaar
             </button>
           </div>
         </form>
